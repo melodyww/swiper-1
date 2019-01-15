0c096d49869bd73fc8d07f2df927951a3e01df7f
Seamile <lanhuermao@gmail.com>
2019-01-15 11:38:56 +0800

    1. 添加了 ProfileForm 2. 完成了修改个人配置 API

--- user/api.py
+++ user/api.py
@@ -5,6 +5,7 @@ from lib.sms import send_sms
 from common import keys
 from common import errors
 from user.models import User
+from user.forms import ProfileForm
 
 
 def submit_phone(request):
@@ -41,8 +42,15 @@ def get_profile(request):
 
 
 def set_profile(request):
-    '''修改个人资料'''
-    return
+    '''修改个人配置'''
+    form = ProfileForm(request.POST)
+    if form.is_valid():
+        profile = form.save(commit=False)
+        profile.id = request.session['uid']
+        profile.save()
+        return render_json(None)
+    else:
+        return render_json(form.errors, errors.PROFILE_ERR)
 
 
 def upload_avatar(request):
